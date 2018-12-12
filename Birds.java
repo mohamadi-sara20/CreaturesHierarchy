44106f0f7a7acf4e5a6e4fd2be70b2c406c3b271
mohamadi-sara20 <mohamadi.sara20@gmail.com>
2018-12-12 13:18:41 +0330

    update

--- Birds.java
+++ Birds.java
@@ -23,7 +23,7 @@ public class Birds extends Creatures{
 
     @Override
     public String toString(){
-        return String.format("%s: %b    %s: %s    %s: %b    ", "Has feathers:", this.getFeathers(), "Diet", this.getDiet(), "Files", this.getFly());
+        return String.format("%s: %b    %s: %s    %s: %b    ", "Has feathers:", this.getFeathers(), "Diet", this.getDiet(), "Flies", this.getFly());
     }
 
 
