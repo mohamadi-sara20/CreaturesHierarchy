f6c79c483dba7a9e20317255825b89e28e60951c
mohamadi-sara20 <mohamadi.sara20@gmail.com>
2018-12-12 18:13:22 +0330

    updateTest

--- Test.java
+++ Test.java
@@ -5,7 +5,7 @@ public class Test {
     public static void main(String[] args){
 
         Creatures[] creatures = new Creatures[3];
-        Creatures[] compareList = new Creatures[9];
+        Creatures[] compareList = new Creatures[6];
 
         Animals bear = new Animals(Reproduction.VIVIPAROUS, Respiration.LUNGS, Languge.NA, Hair.HAIR, Diet.CARNIVOROUS, Tameness.WILD, false);
         Humans female = new Humans(Reproduction.VIVIPAROUS, Respiration.LUNGS, Languge.UNLIMITED, Hair.HAIR, Age.YOUNG, Gender.FEMALE);
@@ -30,11 +30,6 @@ public class Test {
         Humans compareFemale1 = new Humans(Reproduction.OVIOPAROUS, Respiration.LUNGS, Languge.UNLIMITED, Hair.HAIR, Age.YOUNG, Gender.FEMALE);
         Birds compareBird1 = new Birds(Reproduction.VIVIPAROUS, Respiration.LUNGS, Languge.LIMITED, true, Diet.HERBIVOROUS, true);
 
-        //Objects differing in more than one characteristic
-        Animals compareBear2 = new Animals(Reproduction.VIVIPAROUS, Respiration.GILLS, Languge.UNLIMITED, Hair.FUR, Diet.CARNIVOROUS, Tameness.DOMESTIC, true);
-        Humans compareFemale2 = new Humans(Reproduction.VIVIPAROUS, Respiration.INTEGUMENTARY, Languge.LIMITED, Hair.FUR, Age.INFANT, Gender.MALE);
-        Birds compareBird2 = new Birds(Reproduction.OVIOPAROUS, Respiration.GILLS, Languge.NA, false, Diet.CARNIVOROUS, false);
-
 
         compareList[0] = compareBear;
         compareList[1] = compareFemale;
@@ -42,10 +37,6 @@ public class Test {
         compareList[3] = compareBear1;
         compareList[4] = compareFemale1;
         compareList[5] = compareBird1;
-        compareList[6] = compareBear2;
-        compareList[7] = compareFemale2;
-        compareList[8] = compareBird2;
-
 
 
 
