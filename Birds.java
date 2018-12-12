e455288957d858e16a3a86b49677506d91dbfad1
mohamadi-sara20 <mohamadi.sara20@gmail.com>
2018-12-12 13:57:37 +0330

    h

--- Birds.java
+++ Birds.java
@@ -21,6 +21,7 @@ public class Birds extends Creatures{
     public boolean getFeathers(){return this.feathers;}
     public boolean getFly(){return this.fly;}
 
+
     @Override
     public String toString(){
         return super.toString() + String.format("%10s: %10b    %10s: %10s    %10s: %10b    ", "Has feathers:", this.getFeathers(), "Diet", this.getDiet(), "Flies", this.getFly());
